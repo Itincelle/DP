b426c79db877314e43e12849bd9beb16d88d9fdf
ayoubM2i <ayo.daif@gmail.com>
2019-09-28 19:26:19 +0200

    lazy loaded

--- Nouveau dossier/design pattern/src/singleton/DbSingleton.java
+++ Nouveau dossier/design pattern/src/singleton/DbSingleton.java
@@ -1,5 +1,16 @@
 package singleton;
 
 public class DbSingleton {
+	private static DbSingleton instance = null;
+	private DbSingleton() {
+		if(instance!=null) throw new RuntimeException("use getInstance methode for instanciation");
+		
+	}
+	
+	public static DbSingleton getInstance() {
+		if(instance == null) instance= new DbSingleton();
+		return instance;
+	}
+	
 
 }
